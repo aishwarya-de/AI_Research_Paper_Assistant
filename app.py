9b947b642b8c02c1a333eb2035e7655c72b98ef1
Ashikanayak <ashikagajanannayak24cs@rnsit.ac.in>
2026-08-04 13:01:16 +0530

    design image

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 import streamlit as st
+import os
 
-st.title(" AI Research Paper Assistant")
+st.set_page_config(
+    page_title="AI Research Paper Assistant",
+    page_icon="📄",
+    layout="wide"
+)
 
-st.write("Welcome to my AI/ML Project!")
+st.title("AI Research Paper Assistant")
+st.write("Upload a research paper (PDF) to extract and view its text.")
+
+st.sidebar.title("Navigation")
+st.sidebar.write("Upload your research paper from here.")
+
+uploaded_file = st.sidebar.file_uploader(
+    "Upload Research Paper",
+    type=["pdf"]
+)
+
+os.makedirs("uploads", exist_ok=True)
+
+if uploaded_file is not None:
+    file_path = os.path.join("uploads", uploaded_file.name)
+
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.success("PDF uploaded and saved successfully!")
